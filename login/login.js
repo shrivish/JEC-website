6dcc49041e87817daab26e1f61e256a4010726f8
shrivish <shristi.v2002@gmail.com>
2023-02-04 15:15:29 +0530

    js working

---
+++ login/login.js
@@ -0,0 +1,39 @@
+
+let captcha;
+let alphabets = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+console.log(alphabets.length);
+let status = document.getElementById('status');
+//status.innerText = "Captcha Generator 123";
+
+ generate = () => {
+// console.log(status)
+let first = alphabets[Math.floor(Math.random() * alphabets.length)];
+let second = Math.floor(Math.random() * 10);
+let third = Math.floor(Math.random() * 10);
+let fourth = alphabets[Math.floor(Math.random() * alphabets.length)];
+let fifth = alphabets[Math.floor(Math.random() * alphabets.length)];
+let sixth = Math.floor(Math.random() * 10);
+captcha = first.toString()+second.toString()+third.toString()+fourth.toString()+fifth.toString()+sixth.toString();
+console.log(captcha);
+document.getElementById('generated-captcha').value = captcha;
+document.getElementById("entered-captcha").value = '';
+//status.innerText = "Captcha Generator"
+}
+
+ check = () => {
+// console.log(status)
+let userValue = document.getElementById("entered-captcha").value;
+console.log(captcha);
+console.log(userValue);
+if(userValue == captcha){
+    status.innerText = "Correct!!"
+}else{
+    status.innerText = "Invalid Captcha !! Try Again"
+    document.getElementById("entered-captcha").value = '';
+}
+}
+
+
+
+
+
